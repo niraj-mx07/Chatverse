52e06f7849c58d22177e1e56b2b88674c484b684
niraj-mx07 <nirajmahajan61452@gmail.com>
2026-07-04 15:00:53 +0530

    llm.py

--- chatverse-backend/app/core/llm.py
+++ chatverse-backend/app/core/llm.py
@@ -1,1 +1,15 @@
-# TODO: implement LLM client initialization (OpenAI / Google) , soon ... (imortal)
+from langchain_openai import ChatOpenAI , OpenAIEmbeddings
+from app.config import settings
+
+def get_llm(tempreture: float = 0.2) -> ChatOpenAI:
+    return ChatOpenAI (
+        model = "gpt-40-mini"
+        temperature = temperature,
+        api_key = settings.openai_api_key,
+    )
+
+def get_embeddings() -> OpenAIEmbeddings:
+    return OpenAIEmbeddings(
+        model = "text-embedding-3-small",
+        api_key = settings.openai_api_key,
+    )
