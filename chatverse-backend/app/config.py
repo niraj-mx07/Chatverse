3d271ea69a0a49f8a34f67b1b7d447e419033221
niraj-mx07 <nirajmahajan61452@gmail.com>
2026-07-08 13:51:06 +0530

    api chaange

--- chatverse-backend/app/config.py
+++ chatverse-backend/app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    openai_api_key: str = ""
+    google_api_key: str = "AIzaSyBZ9STx-77zFXpyaIgplmJzZVEfd6t1bL8"
     chroma_persist_dir: str = "./chroma_db"
     github_token: str = ""
     gmail_credentials_path: str = "./credentials.json"
